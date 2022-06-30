6a3c3bd7989e05ad8f0f2658bbe222fa34bee3a2
ProgmanZ <dumphill@gmail.com>
2022-06-30 16:56:48 +0300

    Task 03 - Готово.

--- Task-03.py
+++ Task-03.py
@@ -1,19 +1,10 @@
 # Задача 3. Ряд Фибоначчи
 
-def fibo_func(number_iter, first_numb=0, second_numb=1):
-    result = [1]
-    while len(result) != number_iter:
-        result.append(first_numb + second_numb)
-        first_numb += second_numb
-        result.append(first_numb)
-        second_numb += first_numb
-    return result
+def fibonacci(itr, buf=0, numb=1, count=-1):
+    if count == itr:
+        return numb
+    else:
+        return fibonacci(itr, buf + numb, buf, count + 1)
 
 
-print(fibo_func(8))
-
-# 1, 1, 2, 3, 5, 8, 13
-
-#  first: 0, 1, 2, 3, 5
-# second: 1, 2, 3, 5, 8
-
+print('Число:', fibonacci(int(input('Введите позицию числа в ряде Фибоначчи: '))))
