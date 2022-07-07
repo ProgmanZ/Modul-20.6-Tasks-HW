12f6c2a347e9b17c35860c4479d69082b32ced5f
ProgmanZ <dumphill@gmail.com>
2022-07-07 16:11:46 +0300

    Task 04 - Готово.

--- Task-05.py
+++ Task-05.py
@@ -1,22 +1,19 @@
 # Задача 5. Ускоряем работу функции
 
-def calculating_math_func(data, result=dict):
-    if data in result:
-        return result[data]
+def rec_num(number, user_dict=None):
+    if user_dict is None:
+        user_dict = dict()
+    if number == 1:
+        user_dict[number] = number
+        return number
     else:
-        result = ()
-    for index in range(1, data + 1):
-        result *= index
-    result /= data ** 3
-    result = result ** 10
-    return result
+        user_dict[number] = number * rec_num(number - 1, user_dict)
+        return user_dict[number]
 
 
-'''
-{
-1: 1,
-2: 2,
-3: 6,
-4: 7}
-'''
-
+numb = int(input('Введите число: '))
+user_dict = dict()
+rec_num(numb, user_dict)
+user_dict = {key: (user_dict[key]/key**3)**10 for key in user_dict}
+print(user_dict[numb])
+print(user_dict)
